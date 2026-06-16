5a3f2cb345ef24716a6d771e85d71f3dd5089dab
wesley <isabelly250@gmail.com>
2026-06-16 16:29:20 -0300

    Ajuste de portas e variaveis de ambiente

--- clinica/database/connection.js
+++ clinica/database/connection.js
@@ -1,24 +1,20 @@
 const mysql = require("mysql2")
 
 const connection = mysql.createConnection({
-
-    host: "127.0.0.1",
-    user: "root",
-    password: "",   // senha vazia
-    database: "clinica",
-    port: 3306
-
+    host: process.env.DB_HOST || "127.0.0.1",
+    user: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || "",   // senha vazia local
+    database: process.env.DB_NAME || "clinica",
+    port: process.env.DB_PORT || 3306
 })
 
 connection.connect((err) => {
-
     if(err){
         console.log("Erro ao conectar no banco")
         console.log(err)
     }else{
         console.log("Conectado ao MySQL!")
     }
-
 })
 
 module.exports = connection
